eeab36cb90ae2b79c32a0b9eca4c2e83780beaf1
tom.hsu <tom.hsu@baifu-tech.net>
2025-06-12 17:16:47 +0800

    測試2

--- Package.swift
+++ Package.swift
@@ -17,9 +17,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+//        .binaryTarget(
+//            name: "LinkDeepSDK",
+//            path: "Sources/LinkDeepSDK/LinkDeepSDK.xcframework"
+//        ),
         .binaryTarget(
             name: "LinkDeepSDK",
-            path: "Sources/LinkDeepSDK/LinkDeepSDK.xcframework"
+            url: "https://github.com/balvenie-ios/LinkDeepSDK-ios/releases/download/1.0.1/LinkDeepSDK.xcframework.zip",
+            checksum: "123"
         ),
         .testTarget(
             name: "LinkDeepSDKTests",
