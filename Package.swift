1e79bf9538e2024a33902b12fe2fb89db4195406
GitLab CI <ci@example.com>
2025-06-13 18:27:25 +0800

    Update Package.swift for 3.0.0

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         .binaryTarget(
             name: "LinkDeepSDK",
-            url: "https://github.com/balvenie-ios/LinkDeepSDK-ios/releases/download/2.0.6/LinkDeepSDK.xcframework.zip",
-            checksum: "fdbe1d7540d19b540330ee8c071ae8014df41a0e24a89db7db62b7f2b5b650f9"
+            url: "https://github.com/balvenie-ios/LinkDeepSDK-ios/releases/download/3.0.0/LinkDeepSDK.xcframework.zip",
+            checksum: "77f4dfe92467cf88df0fca0c959e0983253f2097a2cd017af6d62f815303046e"
         )
     ]
 )
